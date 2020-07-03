e3ce1e9e62be9bbc12cf0bb9e559985f6941010f
unknown <sabuj.cse.diu@gmail.com>
2020-07-03 15:41:18 +0200

    updated api

--- backend/src/boomjango/urls.py
+++ backend/src/boomjango/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('api/',include('articles.api.urls'))
+    path('api/articles/',include('articles.api.urls')),
+    path('api/products/',include('products.api.urls'))
 ]
